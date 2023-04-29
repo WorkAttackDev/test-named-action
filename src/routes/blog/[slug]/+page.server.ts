5edbb47058d60e0c23f108a85ed43e8fcea7ab90
WorkAttack <workattackgeral@gmail.com>
2023-04-29 02:44:35 +0100

    finished test

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,7 +1,13 @@
+import { json } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions = {
     comment: async (event) => {
-        console.log(event);
+        // get form data
+        const data = Object.fromEntries((await event.request.formData()).entries());
+
+        console.log(data);
+
+        return { data };
     }
 } satisfies Actions;
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,8 +1,14 @@
-
 <script>
-  import Form from "./Form.svelte";
+	import Form from './Form.svelte';
+
+	export let form;
 </script>
+
 <section>
-  Post
-  <Form />
+	Post
+	<Form />
+
+	{#if form?.data}
+		<pre>{JSON.stringify(form.data, null, 2)}</pre>
+	{/if}
 </section>
